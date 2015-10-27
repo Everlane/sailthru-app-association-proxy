5c03c7eb9c9b84e74cc5894bf1ef53d3ff4f695e
Nick Rowe <nixterrimus@dcxn.com>
2015-10-27 15:25:56 -0700

    A working but incomplete deploy- this generates an API but doesn’t serve SSL properly

--- deploy/server-setup.sh
+++ deploy/server-setup.sh
@@ -16,5 +16,17 @@ echo "packer: nginx - installing as a service"
 sudo update-rc.d nginx defaults
 
 echo "packer: nginx - Installing config"
+sudo mkdir -p /etc/nginx/
+sudo mv /tmp/nginx.conf /etc/nginx/sites-enabled
+#sudo chown $INSTANCE_USER /etc/nginx/
+#sudo chmod -r 755 /etc/nginx/
 
 echo "packer: nginx - Install SSL Certificate"
+sudo mkdir -p /etc/ssl/
+sudo mv /tmp/domain.key /etc/ssl/
+sudo mv /tmp/domain.pem /etc/ssl/
+#sudo chown $INSTANCE_USER /etc/ssl/
+#sudo chmod -r 755 /etc/ssl/
+
+echo "packer: reload nginx"
+sudo service nginx reload
